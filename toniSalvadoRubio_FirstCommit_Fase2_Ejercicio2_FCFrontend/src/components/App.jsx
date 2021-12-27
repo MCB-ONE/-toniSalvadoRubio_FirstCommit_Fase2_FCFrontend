c87a7137a153bc0624fde18dff2a2994c88722f5
toni <toni.one.dev@gmail.com>
2021-12-27 19:55:51 +0100

    Ejercicio 2 acabado.

---
+++ toniSalvadoRubio_FirstCommit_Fase2_Ejercicio2_FCFrontend/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomePage from '../pages/home/HomePage';
+
+/**
+ * Función Anónima para crear un Componente principal
+ * @returns {React.Component} Componente principal de nuestra aplicación
+ */
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
